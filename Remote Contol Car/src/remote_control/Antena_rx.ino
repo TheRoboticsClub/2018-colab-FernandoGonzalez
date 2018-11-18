bd23e3296e2044528e8271c1f5086e07db91fd12
Fernando González <39632336+fgonzalezr1998@users.noreply.github.com>
2018-11-18 21:34:43 +0100

    Add files via upload

---
+++ Remote Contol Car/src/remote_control/Antena_rx.ino
@@ -0,0 +1,60 @@
+/*
+ * Receive commands by remote control and it decide the turn angle of front wheels axis
+ * and the back wheels polarity
+ */
+
+#include <VirtualWire.h>
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("setup");
+
+    //Se inicializa el RF
+    vw_setup(2000);  //velocidad: Bits per segundo
+    vw_set_rx_pin(2);
+    vw_rx_start();
+    
+    pinMode(13, OUTPUT);
+    pinMode(7, OUTPUT);
+    digitalWrite(13, false);
+    digitalWrite(7, false);
+}
+
+int vx;
+int vy;
+
+int movement_robot[2]; //Array that defines the robot movement
+
+void loop()
+{
+    uint8_t data[VW_MAX_MESSAGE_LEN];
+    uint8_t dataleng = VW_MAX_MESSAGE_LEN;
+    
+    if (vw_get_message(data,&dataleng)){
+
+        vx = data[0]; //Axis x information (wheels orientation)
+        vy = data[2]; //Axis y information (wheels polarity)
+        //Serial.println(vx);
+        //Serial.println(vy);
+        //Serial.println("----");
+
+    }
+
+    movement_robot[0] = map(vx, 0, 250, 45, 135); //Save the degrees that will move the servomotor
+    
+    if(vy > 130){
+      movement_robot[1] = 1; // 1 if motors have to go forward, and 0 if motors have to go backward
+    }else if(vy < 120){
+      movement_robot[1] = -1;
+       
+    }else{
+      movement_robot[1] = 0;
+    }
+
+    Serial.println(movement_robot[0]);
+    Serial.println(movement_robot[1]);
+    Serial.println("----");
+    
+    delay(200);
+}
---
+++ Remote Contol Car/src/remote_control/Antena_tx.ino
@@ -0,0 +1,33 @@
+/*
+ * To send the Joysticks information by radiofrequency
+ */
+#include <VirtualWire.h>
+
+const int Antenna = 2;
+const int Vy = 0;
+const int Vx = 1;
+
+int dato[2];
+
+void setup() {
+  Serial.begin(9600);
+  
+  vw_setup(2000); //Speed: Bits per second
+  vw_set_tx_pin(Antenna); //Output for the RF 
+}
+
+void loop() {
+  
+    //Read the Joysticks data
+    dato[0] = map(analogRead(Vx), 0, 1024, 0, 255);
+    dato[1] = map(analogRead(Vy), 0, 1024, 0, 255);
+    //Los imprimo por pantalla
+    Serial.println(dato[0]);
+    Serial.println(dato[1]);
+    Serial.println("----");
+    //Send them by the antenna
+    vw_send((uint8_t*)dato, 3);
+    vw_wait_tx();
+    
+    delay(50);
+}
